192973c54533d7e49ecc633f9852ed71ef802bdf
Petr Kamenev <75952058+Pitonixx@users.noreply.github.com>
2022-08-15 12:50:39 +0300

    fix lb

--- main.tf
+++ main.tf
@@ -70,6 +70,14 @@ resource "yandex_lb_network_load_balancer" "lb-1" {
             ip_version = "ipv4"
         }
     }
+    listener {
+        name = "grafana-listener"
+        port = 8080
+        target_port = 30902
+        external_address_spec {
+            ip_version = "ipv4"
+        }
+    }      
     attached_target_group {
         target_group_id = "${yandex_lb_target_group.tg-1.id}"
         healthcheck {
@@ -79,12 +87,4 @@ resource "yandex_lb_network_load_balancer" "lb-1" {
             }
         }
     }
-    listener {
-        name = "grafana-listener"
-        port = 8080
-        target_port = 30902
-        external_address_spec {
-            ip_version = "ipv4"
-        }
-    }      
 }
